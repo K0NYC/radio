e6936a37b50ec5d7c582ac863689ded2f57e32f2
K0NYC <32525647+K0NYC@users.noreply.github.com>
2020-11-30 20:16:41 -0500

    Update rtl-fm.sh

--- rtl-fm.sh
+++ rtl-fm.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-TEMP=$(getopt -o m:f:l:g:p:r:d: --long play,stream,sar,play-sar,file-prefix: -n 'rtl-fm' -- "$@")
+TEMP=$(getopt -o m:f:l:g:p:r:d: --long play,stream,sar,play-sar,filename: -n 'rtl-fm' -- "$@")
 
 if ! rtl-fm; then echo "Terminating..." >&2 ; exit 1 ; fi
 
 eval set -- "$TEMP"
 
-MODE=fm
+MODE="fm"
 FREQUENCY=
 SQUELCH=0
 GAIN=0
 ERROR=0
 SAMPLE_RATE="24k"
 DEVICE=0
-PLAY="play -t raw -r 24k -e s -b 16 -c 1 -V3 - sinc 400-3000"
-STREAM="cvlc -v - --sout '#standard{access=http,mux=ogg,dst=0.0.0.0:8080}'"
-SAR="sox -t raw -r 24k -e s -b 16 -c 1 -V3 - -t mp3 ${filename}.mp3 sinc 400-3000 silence 1 0.1 1% 1 5.0 1% : newfile : restart"
-PLAY_SAR="play -t raw -r 24k -e s -b 16 -c 1 -V3 - | rec -t raw -r 24k -e s -b 16 -c 1 -V3 - -t mp3 ${filename}.mp3 sinc 400-3000 silence 1 0.1 1% 1 5.0 1% : newfile : restart"
-FILE_PREFIX=
+FILENAME=
+PLAY=false
+STREAM=false
+SAR=false
+PLAY_SAR=false
+
+PLAY_CMD=" | play -t raw -r 24k -e s -b 16 -c 1 -V3 - sinc 400-3000"
+STREAM_CMD=" | cvlc -v - --sout '#standard{access=http,mux=ogg,dst=0.0.0.0:8080}'"
+SAR_CMD=" | sox -t raw -r 24k -e s -b 16 -c 1 -V3 - -t mp3 ${FILENAME}.mp3 sinc 400-3000 silence 1 0.1 1% 1 5.0 1% : newfile : restart"
+PLAY_SAR_CMD=" | play -t raw -r 24k -e s -b 16 -c 1 -V3 - | rec -t raw -r 24k -e s -b 16 -c 1 -V3 - -t mp3 ${FILENAME}.mp3 sinc 400-3000 silence 1 0.1 1% 1 5.0 1% : newfile : restart"
 
 while true; do
   case "$1" in
@@ -29,18 +34,18 @@ while true; do
     -r ) SAMPLE_RATE="$2"; shift 2 ;;
     -d ) DEVICE="$2"; shift 2 ;;
     --file-prefix )
-      FILE_PREFIX="$2"; shift 2 ;;
+      FILENAME="$2"; shift 2 ;;
     --play )
-      play; shift 2 ;;
+      PLAY=true; shift 2 ;;
     --stream )
-      stream; shift 2 ;;
+      STREAM=true; shift 2 ;;
     --sar )
-      sar; shift 2 ;;
+      SAR=true; shift 2 ;;
     --play-sar )
-      play-sar; shift 2 ;;
+      PLAY_SAR=true; shift 2 ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
 done
 
-
+" rtl_fm -M ${MODE} -f $
